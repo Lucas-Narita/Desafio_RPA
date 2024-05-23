2bd85c20c9839f9780e42e175e5847ff81fcfec9
Lucas Narita <lucas29freitas@gmail.com>
2024-05-23 15:42:21 -0300

    tabela excel corrigida

--- desafio.js
+++ desafio.js
@@ -1,7 +1,10 @@
 const { Builder, By, until } = require('selenium-webdriver');
+const xlsx = require('node-xlsx');
+const fs = require('fs');
 
 async function scrapeData() {
     let driver = await new Builder().forBrowser('chrome').build();
+    let allData = [];
 
     try {
         // Navegue até a página especificada
@@ -9,7 +12,7 @@ async function scrapeData() {
 
         // Função para extrair dados de todas as linhas de uma página
         async function extractDataFromPage() {
-            let rows = await driver.findElements(By.css('tr.gradeA.odd, tr.gradeA.even'));
+            let rows = await driver.findElements(By.css('tr.gradeA.odd, tr.gradeA.even, tr.gradeC.odd, tr.gradeC.even, tr.gradeX.odd, tr.gradeX.even, tr.gradeU.odd, tr.gradeU.even'));
             for (let row of rows) {
                 let cells = await row.findElements(By.css('td'));
                 let rowData = [];
@@ -17,7 +20,7 @@ async function scrapeData() {
                     let text = await cell.getText();
                     rowData.push(text);
                 }
-                console.log(rowData);
+                allData.push(rowData);
             }
         }
 
@@ -30,15 +33,20 @@ async function scrapeData() {
         // Navegar para a segunda página e extrair dados
         let secondPageButton = await driver.findElement(By.css('a.page-link[aria-controls="DataTables_Table_0"][data-dt-idx="2"]'));
         await secondPageButton.click();
-        await driver.wait(until.elementLocated(By.css('tr.gradeA.odd, tr.gradeA.even')), 10000);
+        await driver.wait(until.elementLocated(By.css('tr.gradeA.odd, tr.gradeA.even, tr.gradeC.odd, tr.gradeC.even, tr.gradeX.odd, tr.gradeX.even, tr.gradeU.odd, tr.gradeU.even')), 10000);
         await extractDataFromPage();
 
         // Navegar para a terceira página e extrair dados
         let thirdPageButton = await driver.findElement(By.css('a.page-link[aria-controls="DataTables_Table_0"][data-dt-idx="3"]'));
         await thirdPageButton.click();
-        await driver.wait(until.elementLocated(By.css('tr.gradeA.odd, tr.gradeA.even')), 10000);
+        await driver.wait(until.elementLocated(By.css('tr.gradeA.odd, tr.gradeA.even, tr.gradeC.odd, tr.gradeC.even, tr.gradeX.odd, tr.gradeX.even, tr.gradeU.odd, tr.gradeU.even')), 10000);
         await extractDataFromPage();
 
+        // Criar um buffer com os dados e escrever no arquivo Excel
+        const worksheet = xlsx.build([{ name: 'Data', data: allData }]);
+        fs.writeFileSync('scraped_data.xlsx', worksheet);
+
+        console.log('Data has been written to scraped_data.xlsx');
     } finally {
         await driver.quit();
     }
